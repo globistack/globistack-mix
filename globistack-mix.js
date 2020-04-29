d32892525ef66ba937501830d4d3c604e79b311c
DEV001 <dev001@globiz365.com>
2020-04-29 07:57:11 +0700

    add JavaScript DocBlock

--- globistack-mix.js
+++ globistack-mix.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 
 class GlobistackMix {
+    /**
+     * 
+     * @returns {string}
+     */
     static getConfig() {
         return {
             cleanCss: {
@@ -12,15 +16,30 @@ class GlobistackMix {
             }
         }
     }
+    /**
+     * 
+     * @param {string} string
+     * @returns {string}
+     */
     static ltrim(string) {
         return string.replace(/^\//, '');
     }
+    /**
+     * 
+     * @param {Object} data
+     * @returns {Array}
+     */
     static assetPaths(data) {
         let assetPaths = Object.keys(data.compilation.assets);
         assetPaths = assetPaths.filter(assetPath => assetPath.endsWith('.css') || assetPath.endsWith('.js'));
         assetPaths = assetPaths.map(assetPath => this.ltrim(assetPath));
         return assetPaths;
     }
+    /**
+     * 
+     * @param {Object} data
+     * @returns {void}
+     */
     static optimize(data) {
         const assetPaths = this.assetPaths(data);
         for (const assetPath of assetPaths) {
